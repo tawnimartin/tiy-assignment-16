9739ded18f19140a3f9bb19c4febaa370b4c2fbd
Tawni Martin <tawni@ideate-interactive.com>
2015-02-24 20:18:58 -0500

    Cleaning up and commenting accordian

--- js/app.js
+++ js/app.js
@@ -9,59 +9,53 @@ var App = (function() {
 
   App.prototype = {
 
-    renderSection: function() {
-       
-      var str = "";
+    renderSection: function() {  
+    //str to hold html output    
+    var str = "";
+    _.each(this.data, function(sect) {
+      //get section id, and put in header div data
+      var sectId = sect.section_id;
+      str += "<div class='section-header' data-sect-name='" + sectId + "'>";
 
-      _.each(this.data, function(sect) {
+      //get and add section name
+      var sectName = sect.section_name;
+      str += sectName + "</div>"; 
 
-        //get section id
-        var sectId = sect.section_id;
-        str += "<div class='section-header' data-sect-name='" + sectId + "'>";
-
-        //get section name
-        var sectName = sect.section_name;
-        str += sectName + "</div>"; 
-
-        //get section id and text
-        var sectText = sect.section_text;
-        str += "<div class='section-body' data-sect-name='" + sectId + "'>";  
-        
-        str += sectText + "</div>";
-        
+      //get and add section text, with section id in data as well
+      var sectText = sect.section_text;
+      str += "<div class='section-body' data-sect-name='" + sectId + "'>";  
+      str += sectText + "</div>";
       });
-
+      //close
       str += "</div>"
+      //output in .section container div
       $(".section").html( str );
 
     },
 
     hideAll: function() {
+      //hides all section bodies
       $(".section-body").slideUp();
     },
 
     addListeners: function() {
-      
+      //make this available to the listener function so we can call hideAll()
       var that = this;
-
+      //listener for section headers
       $(".section").on("click", ".section-header", function(e){
-        //e.preventDefault();
         $clicked = $(e.currentTarget);
-
+        //hide all the section-bodies
         that.hideAll();
-
+        //get section id from header data
         var sectID = $clicked.data("sect-name");
-
+        //create a string to reference the specific section in question, by way of data id
         var targetBody = ".section-body[data-sect-name=" + sectID +"]";
-
+        //show with animation!
         $(targetBody).slideDown();
-
-
       });
-
     }
   }
-
+  
   return App;
 
 })();
