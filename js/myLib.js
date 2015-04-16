8696cf93a7a24f9fa8423d71c1ba047fff3e33cc
Tawni Martin <tawni@ideate-interactive.com>
2015-04-15 23:46:04 -0400

    7 functions

--- js/myLib.js
+++ js/myLib.js
@@ -1,19 +1,19 @@
 var minilib = {
-
+	////////////////////////////
 	//string-related functions//
+	////////////////////////////
 
+	//Converts number to string
 	convertNumberToString: function(num) {
 		return num.toString();
 	},
-
-	//get first name
+	//Gets first name
   getFirstName: function(fullName) {
    var nameArray = fullName.split(' ');
    var firstName = nameArray[0];
    return firstName;
   },
-
-  //truncate a string, add elipsis
+  //Truncates a string, adds elipsis
   truncateString: function (string, length){
    if (string.length > length) {
       return string.substring(0,length)+'...';
@@ -21,5 +21,22 @@ var minilib = {
    else
    return string;
 	},
+	//Finds the index number of a character in a string
+	findIndex: function(string, char) {
+	return string.lastIndexOf(char);
+	},
+	//Removes everything after a certain character
+	removeAfter: function(string, char) {
+		return string.substring(0, string.indexOf(char));
+	},
+	//Removes everything before a certain character
+	removeBefore: function(string, char) {
+		indexofChar = this.findIndex(string, char);
+		return string.slice(0, indexofChar);
+	},
+	//Adds an extension you provide
+	appendExtension: function(string, ext) {
+		return string += ext;
+	}
 
 };
